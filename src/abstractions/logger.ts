26456781bea99e6ebf2bd95528a83e7c1fa15499
Nick Sharp <nick.sharp.87@gmail.com>
2021-09-14 20:22:17 +0100

    fix: fixing logger

--- src/abstractions/logger.ts
+++ src/abstractions/logger.ts
@@ -1,10 +1,10 @@
 import { ClassType } from '../class-type';
 
 export interface ILogger {
-  debug(message: string, ...args: any[]): Promise<void>;
-  info(message: string, ...args: any[]): Promise<void>;
-  warn(message: string, ...args: any[]): Promise<void>;
-  error(error: Error, ...args: any[]): Promise<void>;
+  debug(message: string, ...args: any[]): void;
+  info(message: string, ...args: any[]): void;
+  warn(message: string, ...args: any[]): void;
+  error(error: Error, ...args: any[]): void;
 }
 
 export const ILogger = class Dummy {} as ClassType<ILogger>;
