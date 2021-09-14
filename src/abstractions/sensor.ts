fec137514428310bcb33a6221dac022e821acac9
Nick Sharp <nick.sharp.87@gmail.com>
2021-09-14 20:08:26 +0100

    fix: using messaging manager in sensor

--- src/abstractions/sensor.ts
+++ src/abstractions/sensor.ts
@@ -1,5 +1,5 @@
-import { SocketIo } from '@overtheairbrew/socket-io';
 import Container, { Token } from 'typedi';
+import { IMessagingManager } from '.';
 import { SensorReading } from '../messages/sockets/sensor-reading';
 import { Property } from '../properties';
 import { Validatable } from '../properties/validatable';
@@ -13,16 +13,16 @@ export interface ISensor {
 }
 
 export abstract class Sensor extends Validatable implements ISensor {
-  private socket: SocketIo;
+  private messagingManager: IMessagingManager;
 
   constructor(public sensorType: string, public properties: Property[]) {
     super(properties);
 
-    this.socket = Container.get<SocketIo>(SocketIo);
+    this.messagingManager = Container.get<IMessagingManager>(IMessagingManager);
   }
 
   protected async dataRecieved(sensor_id: string, value: number) {
-    await this.socket.send(SensorReading)({
+    await this.messagingManager.sendMessage(SensorReading)({
       sensor_id,
       value,
     });
