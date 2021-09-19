5c102b5b0b4eac73c68be6730b9881eab8b69c5a
Nick Sharp <nick.sharp.87@gmail.com>
2021-09-19 19:51:56 +0100

    fix: allowing select box to be loaded via a server function

--- src/properties/select-box.ts
+++ src/properties/select-box.ts
@@ -1,13 +1,29 @@
+import Container from 'typedi';
+import { ILogger } from '..';
 import { Property } from './base-property';
 
 export class SelectBoxProperty<T extends string | number> extends Property {
   constructor(
     public name: string,
-    public values: T[],
     public defaultValue: T,
     public required: boolean,
+    public values?: T[],
+    public serverLoad?: () => Promise<T[]>,
   ) {
     super('select-box');
+
+    if (typeof serverLoad !== undefined && values.length) {
+      const logger = Container.get(ILogger);
+      logger.warn(`Ignoring values as plugin is set to load from the server`);
+    }
+  }
+
+  public async getValues(): Promise<T[]> {
+    if (typeof this.serverLoad !== 'undefined') {
+      return await this.serverLoad();
+    }
+
+    return this.values;
   }
 
   public async validateProperty(value: T | undefined) {
